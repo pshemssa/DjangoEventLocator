68680f5bdc6daf64d70015723f0e5b72f9856b31
pshemssa <s.patience.com>
2025-03-12 16:46:12 +0200

    docker

--- DjangoEventLocator/settings.py
+++ DjangoEventLocator/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'DjangoEventLocator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'file_manager',
+        'USER': 'root',
+        'PASSWORD': 'S#emssa0123',
+        'HOST': 'localhost',
+        'PORT': '8000',
     }
 }
 
